9b0898785984238884c06f37707ddd9c43570ebf
Gavin Williams <fatmcgav@gmail.com>
2016-04-08 10:18:52 +0100

    Update admin create exec to check 99-couchdb.ini file for admin accounts

--- manifests/admin.pp
+++ manifests/admin.pp
@@ -5,9 +5,8 @@ define couchdb::admin (
 ){
 
   exec { 'couchdb admin':
-      command => "/usr/bin/curl -X PUT http://localhost:5984/_config/admins/${title} -d '\"${password}\"'",
-      unless  => "/bin/grep ^${title} ${couchdb::couchdb_conf_dir}/local.ini",
-      require     => Class['couchdb::service'],
-
+    command => "/usr/bin/curl -X PUT http://localhost:5984/_config/admins/${title} -d '\"${password}\"'",
+    unless  => "/bin/grep ^${title} ${couchdb::couchdb_conf_dir}/local.d/99-couchdb.ini",
+    require => Class['couchdb::service'],
   }
 }
